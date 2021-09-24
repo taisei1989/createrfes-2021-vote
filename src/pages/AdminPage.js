275ca3f74cc5a8ad62d6cea61161ca03abd7a94b
taisei yamashita <taisei6514@gmail.com>
2021-09-24 11:23:02 +0900

    データ一覧表示機能を追加

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,47 +1,56 @@
-import React, { useState } from "react";
-import { ref, set } from "firebase/database";
+import React, { useEffect, useState } from "react";
+import { ref, set, onValue } from "firebase/database";
 import { db } from "../services/Firebase";
 
 const AdminPage = () => {
   const [topic, setTopic] = useState("");
   const [answerA, setAnswerA] = useState("");
   const [answerB, setAnswerB] = useState("");
-  
+  const [data, setData] = useState([]);
+
   const handleNewTopic = (event) => {
-    console.log(event.target.value);
     setTopic(event.target.value);
   }
   const handleNewAnswerA = (event) => {
-    console.log(event.target.value);
     setAnswerA(event.target.value);
   }
   const handleNewAnswerB = (event) => {
-    console.log(event.target.value);
     setAnswerB(event.target.value);
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // topicId は自動生成する
     let topicId = Math.random().toString(32).substring(2);
     if (topic === "" || answerA === "" || answerB === "") {
       console.log("回答されていない箇所があります");
       return
     }
     set(ref(db, 'topics/' + topicId), {
-      topicText: topic,
-      topicAnswerA: answerA,
-      topicAnswerB: answerB,
-    });
+        topicText: topic,
+        topicAnswerA: answerA,
+        topicAnswerB: answerB,
+      },
+    );
+    setTopic("");
+    setAnswerA("");
+    setAnswerB("");
   }
-  
+
+  useEffect(() => {
+    const topicRef = ref(db, 'topics/');
+    onValue(topicRef, (snapshot) => {
+      const data = Object.values(snapshot.val());
+      setData(data);
+    });
+  }, []);
+
   return (
     <div>
       <h1>Admin Page</h1>
       <form onSubmit={handleSubmit}>
         <label>
-          タイトル：
-          <input type="text" value={ topic } placeholder="お題のタイトルを記入"
+          お題：
+          <input type="text" value={ topic } placeholder="お題を記入"
             onChange={handleNewTopic} />
         </label><br/>
         <label>
@@ -56,6 +65,15 @@ const AdminPage = () => {
         </label><br/>
         <button type="submit">送信</button>
       </form>
+      <p></p><br/>
+      <h2>お題と回答一覧</h2>
+      {data.map((data) => (
+        <ul>
+          <li>お題：{data.topicText}</li>
+          <li>回答A：{data.topicAnswerA}</li>
+          <li>回答B：{data.topicAnswerB}</li>
+        </ul>
+      ))}
     </div>
   );
 };
