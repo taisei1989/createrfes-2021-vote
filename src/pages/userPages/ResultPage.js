bf5870ae8d6703eea397e14c0b5282775d2acccd
ichi hirota <ghiroichi@gmail.com>
2021-09-29 22:14:08 +0900

    gitの設定によって大文字小文字のパスがずれていた問題をできる限り修正

---
+++ src/pages/userPages/ResultPage.js
@@ -0,0 +1,9 @@
+const ResultPage = () => {
+  return (
+    <div>
+      <h1>ResultPage</h1>
+    </div>
+  );
+};
+
+export default ResultPage;
---
+++ src/pages/userPages/TallyPage.js
@@ -0,0 +1,9 @@
+const TallyPage = () => {
+  return (
+    <div>
+      <h1>TallyPage</h1>
+    </div>
+  );
+};
+
+export default TallyPage;
