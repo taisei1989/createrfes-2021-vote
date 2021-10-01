d45b8761a737fb4920d132a5d305ad814ea7b5a0
ichi hirota <ghiroichi@gmail.com>
2021-10-01 16:54:40 +0900

    scssの導入。仮で司会者ページにstyleを当てる

--- src/pages/moderator/TopicAnswerPanel.js
+++ src/pages/moderator/TopicAnswerPanel.js
@@ -1,3 +1,5 @@
+import styles from './TopicAnswerPanel.module.scss';
+
 /**
  * お題と答えを表示するパネル
  */
@@ -5,10 +7,10 @@ const TopicAnswerPanel = () => {
   // ここでお題と答えを取得する処理
 
   return (
-    <div>
-      <div>お題</div>
-      <div>答えA</div>
-      <div>答えB</div>
+    <div className={styles.topicAnswerPanel}>
+      <div className={styles.topic}>お題</div>
+      <div className={styles.answerA}>答えA</div>
+      <div className={styles.answerB}>答えB</div>
     </div>
   );
 };
