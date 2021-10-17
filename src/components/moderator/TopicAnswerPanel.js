dce3d059505e8d9b5bf31479eb2a8e9e39487337
ichi hirota <ghiroichi@gmail.com>
2021-10-17 17:43:43 +0900

    集計結果の表示するロジックを追加

--- src/components/moderator/TopicAnswerPanel.js
+++ src/components/moderator/TopicAnswerPanel.js
@@ -7,7 +7,19 @@ import { PHASES } from "../../interfaces";
 
 const isDebug = CONF.IS_DEBUG && true;
 
-/**
+/** -----------------------------------
+ * 答えの%を表示する
+ */
+const Result = ({ phase, perAnswer }) => {
+  if (phase === PHASES.TALLY) {
+    return <p className={styles.result}>？？ %</p>;
+  } else if (phase === PHASES.RESULT) {
+    return <p className={styles.result}>{perAnswer} %</p>;
+  }
+  return null;
+};
+
+/** -----------------------------------
  * お題と答えを表示するパネル
  */
 const TopicAnswerPanel = ({ phase }) => {
@@ -16,22 +28,18 @@ const TopicAnswerPanel = ({ phase }) => {
     answerA: "",
     answerB: "",
   });
+  const [perAnswers, setPerAnswers] = useState({
+    perAnswerA: 50,
+    perAnswerB: 50,
+  });
 
-  const dispTopic =
-    phase === PHASES.VOTE || phase === PHASES.TALLY || phase === PHASES.RESULT
-      ? topic
-      : {
-          topicText: "お題を準備しています",
-          answerA: "？？？",
-          answerB: "？？？",
-        };
-
-  // Connect to Firebase Database
+  // get Topic and Answer
   useEffect(() => {
+    // Firebase Database
     const db = getDatabase();
     const refCurrent = ref(db, "current/");
 
-    const unsubscribe = onValue(refCurrent, (snapshot) => {
+    const unsubscribeCurrent = onValue(refCurrent, (snapshot) => {
       const topicText = snapshot.val().currentTopicText;
       const answerA = snapshot.val().currentAnswerA;
       const answerB = snapshot.val().currentAnswerB;
@@ -51,10 +59,63 @@ const TopicAnswerPanel = ({ phase }) => {
 
     // コンポーネントがアクティブでなくなったらクリーンナップとして接続を解除する
     return () => {
-      unsubscribe();
+      unsubscribeCurrent();
     };
   }, []);
 
+  // get num of answers
+  useEffect(() => {
+    const db = getDatabase();
+    const votesRef = ref(db, "votes/");
+
+    const unsubscribeVotes = onValue(votesRef, (snapshot) => {
+      const votesUpdated = Object.values(snapshot.val());
+
+      // 集計結果
+      const numOfVotes = { numOfVoteA: 0, numOfVoteB: 0 };
+
+      for (let i = 0; i < votesUpdated.length; i++) {
+        const answer = votesUpdated[i].answer;
+
+        // undefinedチェック
+        if (answer) {
+          if (answer === "A") {
+            numOfVotes.numOfVoteA++;
+          } else if (answer === "B") {
+            numOfVotes.numOfVoteB++;
+          } else {
+            if (isDebug) console.error("不明な投票を検知しました", answer);
+          }
+        }
+      }
+
+      let votesLength = numOfVotes.numOfVoteA + numOfVotes.numOfVoteB;
+      if (votesLength === 0) votesLength = 1;
+      const perAnswerA = Math.round(
+        (numOfVotes.numOfVoteA / votesLength) * 100
+      );
+      const perAnswerB = Math.round(
+        (numOfVotes.numOfVoteB / votesLength) * 100
+      );
+
+      setPerAnswers({ perAnswerA, perAnswerB });
+    });
+
+    return () => {
+      unsubscribeVotes();
+    };
+  }, []);
+
+  const dispTopic =
+    phase === PHASES.VOTE || phase === PHASES.TALLY || phase === PHASES.RESULT
+      ? topic
+      : {
+          topicText: "お題を準備しています",
+          answerA: "？？？",
+          answerB: "？？？",
+        };
+
+  // Render
   return (
     <div className={styles.topicAnswerPanel}>
       <div className={styles.topicText}>{dispTopic.topicText}</div>
@@ -63,11 +124,13 @@ const TopicAnswerPanel = ({ phase }) => {
         <div className={styles.answerA}>
           <p className={styles.choice}>A</p>
           <p className={styles.answer}>{dispTopic.answerA}</p>
+          <Result phase={phase} perAnswer={perAnswers.perAnswerA} />
         </div>
 
         <div className={styles.answerB}>
           <p className={styles.choice}>B</p>
           <p className={styles.answer}>{dispTopic.answerB}</p>
+          <Result phase={phase} perAnswer={perAnswers.perAnswerB} />
         </div>
       </div>
     </div>
