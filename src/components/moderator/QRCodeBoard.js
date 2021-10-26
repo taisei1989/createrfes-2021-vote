5432d0f5dccf90024484c66cef3d6f727b4240d4
taisei yamashita <taisei6514@gmail.com>
2021-10-26 11:39:57 +0900

    準備中画面でのQRコードボードの拡大縮小

--- src/components/moderator/QRCodeBoard.js
+++ src/components/moderator/QRCodeBoard.js
@@ -1,12 +1,32 @@
+import { useRef } from "react";
+import { PHASES } from "../../interfaces";
 import styles from "./ModeratorCommon.module.scss";
+import { CSSTransition } from "react-transition-group";
 //import { PHASES } from "../../interfaces";
 
 const QRCodeBoard = ({ phase }) => {
   // TODO: Prepareフェーズの時は、QRコードを大きく表示
+  const isShown = phase === PHASES.PREPARE;
+  const elementRef = useRef(null);
+
   return (
-    <div className={styles.qrCodeBoard}>
-      <img src="images/moderator/qr-code-board.png" alt="" />
-    </div>
+    <CSSTransition
+      in={isShown}
+      nodeRef={elementRef}
+      timeout={1000}
+      classNames={{
+        enter: styles.qrCodeEnter,
+        enterActive: styles.qrCodeEnterActive,
+        enterDone: styles.qrCodeEnterDone,
+        exit: styles.qrCodeExit,
+        exitActive: styles.qrCodeExitActive,
+        exitDone: styles.qrCodeExitDone,
+      }}
+    >
+      <div className={styles.qrCodeBoard} ref={elementRef}>
+        <img src="images/moderator/qr-code-board.png" alt="" />
+      </div>
+    </CSSTransition>
   );
 };
 
