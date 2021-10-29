7ebec5eb94f3d0f8e489cc3872e208e8e77bcc99
ichi hirota <ghiroichi@gmail.com>
2021-10-29 16:22:15 +0900

    Pixiにてフィードバックを描写するキャンバスを追加

--- src/components/moderator/GoodBadPanel.js
+++ src/components/moderator/GoodBadPanel.js
@@ -1,22 +1,57 @@
+import { useEffect, useRef, useState } from "react";
+import * as PIXI from "pixi.js";
 import { PHASES } from "../../interfaces";
 import styles from "./ModeratorCommon.module.scss";
+import { IS_DEBUG } from "../../configs";
+
+const isDebug = IS_DEBUG && true;
+
+/**
+ * フィードバックを描写するための処理
+ */
+class DrowFeedback {
+  constructor(divElement) {
+    this.pixiApp = new PIXI.Application({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      transparent: true,
+    });
+
+    divElement.appendChild(this.pixiApp.view);
+  }
+}
 
 /**
  * ユーザーから寄せられたGoodとBadを表示するパネル
  * 渡されたphaseに応じて、動作を変えたり、表示非表示をする責務を負う
  */
 const GoodBadPanel = ({ phase }) => {
-  // TODO: ここにCanvas描写の処理を追加する
+  const divElement = useRef(null);
+  const [drowFeedback, setDrowFeedback] = useState(null);
+
+  useEffect(() => {
+    if (divElement.current) {
+      if (drowFeedback === null) {
+        if (isDebug) console.log("フィードバック描写用のクラスを初期化します");
+
+        const drowFeedbackIns = new DrowFeedback(divElement.current);
+        setDrowFeedback(drowFeedbackIns);
+      }
+    }
+  }, [divElement, drowFeedback]);
 
   // PhaseがResultの時だけ表示
-  if (phase === PHASES.RESULT) {
-    return (
-      <div className={styles.goodBadPanel}>
-        <canvas className={styles.feedbackCanvas} />
-      </div>
-    );
-  }
-  return null;
+  const divStyle =
+    phase === PHASES.RESULT ? { display: "block" } : { display: "none" };
+
+  // render
+  return (
+    <div
+      className={styles.goodBadPanel}
+      style={divStyle}
+      ref={divElement}
+    ></div>
+  );
 };
 
 export default GoodBadPanel;
