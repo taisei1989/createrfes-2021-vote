4411860f10eaf353061ad61cf90f9babcedb07ec
taisei1989 <82256596+taisei1989@users.noreply.github.com>
2021-11-26 12:41:30 +0900

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,13 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMmXUYy2NV7mJTXJAkKpflVZvhWaYNNiY",
-  authDomain: "creator-fes-2021-vote.firebaseapp.com",
-  databaseURL: "https://creator-fes-2021-vote-default-rtdb.firebaseio.com",
-  projectId: "creator-fes-2021-vote",
-  storageBucket: "creator-fes-2021-vote.appspot.com",
-  messagingSenderId: "829880593445",
-  appId: "1:829880593445:web:118f7ec96c506bae6863cc",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export const app = initializeApp(firebaseConfig);
